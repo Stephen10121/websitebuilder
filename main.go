d52b0885fd0b5856d510d3d84fda0df1bec62678
Stephen Gruzin <stephengrzn@gmail.com>
2025-02-11 18:21:06 -0800

    Allowing for multiple http methods

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/pocketbase/dbx"
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/core"
 )
@@ -12,35 +13,27 @@ func main() {
 	app := pocketbase.New()
 
 	app.OnServe().BindFunc(func(se *core.ServeEvent) error {
-		se.Router.GET("/", func(e *core.RequestEvent) error {
-			record, err := app.FindFirstRecordByData("routes", "path", "/")
-			if err != nil {
-				return e.String(http.StatusNotFound, "Page not found.")
-			} else {
-				return e.String(http.StatusOK, record.GetString("serve"))
-			}
-		})
-
-		se.Router.GET("/{name}/", func(e *core.RequestEvent) error {
+		se.Router.Any("/", func(e *core.RequestEvent) error {
 			path := e.Request.URL.Path
-			if path[len(path)-1:] == "/" {
+			if path[len(path)-1:] == "/" && len(path) > 1 {
 				path = path[0 : len(path)-1]
 			}
 
-			record, err := app.FindFirstRecordByData("routes", "path", path)
+			record, err := app.FindFirstRecordByFilter(
+				"routes",
+				"path = {:path} && httpMethod = {:httpMethod}",
+				dbx.Params{"path": path, "httpMethod": e.Request.Method},
+			)
 			if err != nil {
 				return e.String(http.StatusNotFound, "Page not found.")
 			} else {
 				return e.String(http.StatusOK, record.GetString("serve"))
 			}
 		})
-		// serves static files from the provided public dir (if exists)
-		// se.Router.GET("/{path...}", apis.Static(os.DirFS("./pb_public"), false))
 
 		return se.Next()
 	})
 
-	log.Println("Starting the server.")
 	if err := app.Start(); err != nil {
 		log.Fatal(err)
 	}
